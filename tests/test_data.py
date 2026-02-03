45f51fc0455686e4e51c8d22f90e78ee6ab348c2
VaiTon <eyadlorenzo@gmail.com>
2026-02-04 00:19:28 +0100

    Replace '/' operator with joinpath in test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -847,7 +847,7 @@ class TestChallengeFileWrite(unittest.TestCase):
         self.assertIn("handout_dir: files", yml_str)
 
 class TestChallengeFileLoad(unittest.TestCase):
-    file_dir = pathlib.Path(__file__).parent / 'data'
+    file_dir = pathlib.Path(__file__).parent.joinpath('data')
     json_file = 'full-example.json'
     json_multi_flag_file = 'full-example-multi-flag.json'
     json_multi_flag_object_file = 'full-example-multi-flag-object.json'
